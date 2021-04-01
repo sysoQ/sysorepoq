6f26d9e93f7ec2b995d895bf41b8ba89047ab0a1
unknown <emmi.pallaspuro@gmail.com>
2021-04-01 09:39:37 +0300

    first commit

---
+++ src/main/java/com/example/kysely/KyselyApplication.java
@@ -0,0 +1,13 @@
+package com.example.kysely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyselyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyselyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kysely/KyselyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kysely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyselyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
