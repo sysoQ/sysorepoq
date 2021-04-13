206b18f393b7684e93b59c84f186c9864a56c2b0
Noora Rytkönen <bgm056@myy.haaga-helia.fi>
2021-04-13 15:40:16 +0300

    answer repo muokattu

--- src/main/java/com/example/kysely/model/AnswerRepo.java
+++ src/main/java/com/example/kysely/model/AnswerRepo.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 
 
 
-public interface AnswerRepo extends CrudRepository<Question, Long> {  
+public interface AnswerRepo extends CrudRepository<Answer, Long> {  
 
 	//List<Question> findAll();  
 }
