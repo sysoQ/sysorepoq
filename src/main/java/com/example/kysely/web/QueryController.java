d8c3b32852312374dc869ab4c7c6d63a020586bb
Noora Rytkönen <bgm056@myy.haaga-helia.fi>
2021-04-15 10:09:41 +0300

    muokattu querycontrolleria

--- src/main/java/com/example/kysely/web/QueryController.java
+++ src/main/java/com/example/kysely/web/QueryController.java
@@ -1,21 +1,17 @@
 package com.example.kysely.web;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.kysely.model.Query;
 import com.example.kysely.model.QueryRepo;
-import com.example.kysely.model.Question;
 import com.example.kysely.model.QuestionRepo;
 
 
@@ -35,11 +31,11 @@ public class QueryController {
 	    }   
 	 
 
-	 
-	/* @RequestMapping(value="/queries", method = RequestMethod.GET) 
+	@CrossOrigin
+	@RequestMapping(value="/queries", method = RequestMethod.GET) 
 	 public @ResponseBody List<Query> queryListRest(){ 
 		 return (List<Query>)repo.findAll();
-	 }  */
+	 }  
 
      @RequestMapping(value= {"/addQuery"})
 	    public String addQuery(Model model) {	
