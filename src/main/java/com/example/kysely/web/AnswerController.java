26f200de0c57790b38252290a78de127ac5cd105
unknown <emmi.pallaspuro@gmail.com>
2021-04-27 14:27:29 +0300

    post mapping apit

--- src/main/java/com/example/kysely/web/AnswerController.java
+++ src/main/java/com/example/kysely/web/AnswerController.java
@@ -27,7 +27,7 @@ public class AnswerController {
 	private AnswerRepo answerRepository;
 
 	//REST list answers by questions
-	@GetMapping("/questions/{id}/answers")
+	@GetMapping("/api/questions/{id}/answers")
 	public List<Answer> findAnswersByQuestion(@PathVariable("id") Long questionId) {
 		Question question = questionRepository.findById(questionId).orElse(null);
 		return answerRepository.findByQuestion(question);
@@ -35,7 +35,7 @@ public class AnswerController {
 
 	
 	@CrossOrigin
-	@GetMapping("/answers")
+	@GetMapping("/api/answers")
 	public List<Answer> findAllAnswers() {
 		
 		return (List<Answer>) answerRepository.findAll();
@@ -43,7 +43,7 @@ public class AnswerController {
 	}
 	
 	//REST save answer
-		@PostMapping(value = "api/answers")
+		@PostMapping(value = "/api/answers")
 		public List<Answer> saveAnswer(@RequestBody List<Answer> answers) throws Exception {
 			List<Answer> savedAnswers = new ArrayList<Answer>();
 			
